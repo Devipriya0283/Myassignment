7c755995775655ef7ac3551352ed4c7c2045b455
Devipriya0283 <kamalpriya102@gmail.com>
2025-03-29 20:15:22 +0000

    Week1 - Day 1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,41 @@
+package week1.day1;
+
+
+// 	a) Create a class named Chrome with a main method.//
+//b) Inside the main method, declare and initialize the following variables: //
+
+public class Chrome {
+
+	public static void main(String[] args) 
+	{
+						
+		//1. version of type float with a value of 91.0f
+					float a = 91.0F;
+					
+		// 2. developer of type Stringwith a value of "Google"
+			
+					String  b	= "Google";
+						
+		//	3. isBeta of type booleanwith a value of false
+			
+					boolean c = false;
+					
+		// 4. releaseYear of type int with a value of 2008
+			
+					int d = 2008;
+					
+		// 	5. shortcutKey of type charwith a value of 'C'
+			
+					char e ='C';
+			
+		//c) Print the values of all variables using System.out.println. 
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+		//Run the program and confirm that the variable values are printed in the console.
+			
+	}
+
+}
---
+++ week1/day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloTestLeaf 
+{
+	// Main Method
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to TestLeaf");
+	}
+
+}
